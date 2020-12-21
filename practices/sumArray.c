58478682342027266d9fca3f51ce0f71c3e3cbd5
hack-rookie37 <dev.rookie37@gmail.com>
2020-12-21 16:47:50 +0900

    sum of array

--- practices/sumArray.c
+++ practices/sumArray.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    int numArr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int numArr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = 0;
 
-    for(int i = 0; i < sizeof(numArr)/sizeof(int); i++){
+    for (int i = 0; i < sizeof(numArr) / sizeof(int); i++)
+    {
         sum += numArr[i];
     }
     printf("%d\n", sum);
 
     return 0;
-
 }
